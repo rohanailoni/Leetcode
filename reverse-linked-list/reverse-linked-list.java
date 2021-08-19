62ef08368d9fffaa018b8fa1de07db032bb84e2c
rohanailoni <57516241+rohanailoni@users.noreply.github.com>
2021-08-19 07:57:06 +0400

    Update reverse-linked-list.java
    100%

--- reverse-linked-list/reverse-linked-list.java
+++ reverse-linked-list/reverse-linked-list.java
@@ -9,30 +9,18 @@
  * }
  */
 class Solution {
+    private ListNode rev(ListNode head){
+        ListNode pre=null;
+        ListNode temp=null;
+        while(head!=null){
+            temp=head;
+            head=head.next;
+            temp.next=pre;
+            pre=temp;
+        }
+        return pre;
+    }
     public ListNode reverseList(ListNode head) {
-        List<Integer>n=new ArrayList<>();
-        ListNode tail=head;
-        while(tail!=null){
-            n.add(tail.val);
-            tail=tail.next;
-            
-        }
-        int i=0;int j=n.size()-1;
-        while(i<j){
-            int temp=n.get(i);
-            n.set(i,n.get(j));
-            n.set(j,temp);
-            i++;
-            j--;
-        }
-        
-        int k=0;
-        tail=head;
-        while(tail!=null){
-            tail.val=n.get(k);
-            tail=tail.next;
-            k++;
-        }
-        return head;
+        return rev(head);
     }
 }
