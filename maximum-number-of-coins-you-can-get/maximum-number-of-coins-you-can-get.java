0381497afcb335206d109a319b9c3c2ff2619687
rohanailoni <57516241+rohanailoni@users.noreply.github.com>
2021-10-01 12:12:13 +0530

    Time: 1064 ms (5.10%), Space: 49.8 MB (37.54%) - LeetHub

--- maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.java
+++ maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.java
@@ -1,6 +1,48 @@
 class Solution {
+    private void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        return;
+    }
+    private int parition(int arr[],int i,int j){
+        int ref=i;
+        int pivot=arr[j];
+        for(int k=i;k<j;k++){
+            if(arr[k]<pivot){
+                swap(arr,ref,k);
+                ref++;
+            }
+        }
+        swap(arr,j,ref);
+        return ref;
+    }
+    
+    
+    
+    
+    private void quicksort(int[] nums,int i,int j){
+        if(i<j){
+            int par=parition(nums,i,j);
+            
+            
+            quicksort(nums,0,i-1);
+            quicksort(nums,i+1,j);
+        }
+    }
+    
+    
+    
+    
+    
+    
     public int maxCoins(int[] piles) {
         Arrays.sort(piles);
+        //quicksort(piles,0,piles.length-1);
+        for(int i:piles){
+            System.out.print(i);
+        }
+        System.out.println();
         List<Integer>a=new ArrayList<>();
         for(int i:piles)a.add(i);
         int sum=0;
