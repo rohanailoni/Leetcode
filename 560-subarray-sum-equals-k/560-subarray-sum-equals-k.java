13fda4b1a0930e51228a7c398c47c8e6bf6cdd4d
rohanailoni <57516241+rohanailoni@users.noreply.github.com>
2022-02-10 09:43:12 +0530

    Time: 16 ms (98.76%), Space: 44.8 MB (64.26%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -5,10 +5,12 @@ class Solution {
         int count=0;
         map.put(0,1);
         int currsum=0;
+        int def;
         for(int i=0;i<nums.length;i++){
             currsum+=nums[i];
-            if(map.containsKey(currsum-k)){
-                count+=map.get(currsum-k);
+            def=currsum-k;
+            if(map.containsKey(def)){
+                count+=map.get(def);
             }
             map.put(currsum,map.getOrDefault(currsum,0)+1);
         }
