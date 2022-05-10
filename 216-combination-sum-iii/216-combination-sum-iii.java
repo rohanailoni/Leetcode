6aa641054ffc99becb7747d5bdb24baffc639572
rohanailoni <57516241+rohanailoni@users.noreply.github.com>
2022-05-10 16:51:38 +0530

    Time: 1 ms (73.61%), Space: 42.1 MB (11.99%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -5,11 +5,11 @@ class Solution {
         ans=new ArrayList<>();
         this.k=k;
         this.n=n;
-        dfs(0,0,new ArrayList<>());
+        dfs(0,0,new ArrayList<>(),0);
         return ans;
     }
-    public void dfs(int i,int sum,List<Integer>cache){
-        //System.out.println(cache);
+    public void dfs(int i,int sum,List<Integer>cache,int size){
+        //System.out.println(ans);
         if(cache.size()==this.k && sum==this.n){
             ans.add(new ArrayList<>(cache));
             return;
@@ -20,8 +20,8 @@ class Solution {
         
         for(int j=i+1;j<10;j++){
             cache.add(j);
-            dfs(j,sum+j,cache);
-            cache.remove(cache.size()-1);
+            dfs(j,sum+j,cache,size+1);
+            cache.remove(size);
         }
         return;
     }
