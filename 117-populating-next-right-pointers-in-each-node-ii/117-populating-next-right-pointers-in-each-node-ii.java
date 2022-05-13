1b76c88f480b7b95940cf7716e8f2e3995551917
rohanailoni <57516241+rohanailoni@users.noreply.github.com>
2022-05-14 01:41:46 +0530

    Time: 1 ms (71.78%), Space: 42.4 MB (73.00%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -1,31 +1,52 @@
-// class queue<T>{
+class node<T>{
+    T obj;
+    node next;
+    node(T obj){
+        this.obj=obj;
+        next=null;
+    }
     
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+class queue<T>{
+    node<T> head;
+    node<T> tail;
+    int count;
+    queue(){
+        head=null;
+        tail=null;
+        count=0;
+    } 
+    T peek(){
+        return head.obj;
+    }
+    T pop(){
+        count--;
+        node<T> temp=head;
+        
+        head=head.next;
+        return temp.obj;
+    }
+    void add(T n){
+        count++;
+        if(head==null){
+            node<T> n1=new node<>(n);
+            head=n1;
+            tail=n1;
+            return;
+        }else{
+            node<T>n1=new node<>(n);
+            tail.next=n1;
+            tail=n1;
+            
+        }
+    }
+    int size(){
+        return count;
+    }
+    boolean isEmpty(){
+        return head==null?true:false;
+    }
+}
 
 
 class Solution {
@@ -33,13 +54,12 @@ class Solution {
         if(root==null){
             return root;
         }
-        Queue<Node>qu=new ArrayDeque<>();
+        queue<Node>qu=new queue<Node>();
         qu.add(root);
-        
         while(!qu.isEmpty()){
             int count=qu.size();
             for(int i=0;i<count;i++){
-                Node aa=qu.remove();
+                Node aa=qu.pop();
                 //System.out.println(aa.val);
                 if(qu.size()>=1 && i!=count-1){
                     aa.next=qu.peek();
