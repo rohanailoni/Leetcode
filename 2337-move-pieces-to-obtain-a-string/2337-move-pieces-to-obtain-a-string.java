d788f66fc17f55068c0a54d067a1275bd1566d2c
rohanailoni <57516241+rohanailoni@users.noreply.github.com>
2022-07-10 10:41:10 +0530

    Time: 330 ms (11.11%), Space: 107.9 MB (11.11%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        if (!(start.replaceAll("_", "")).equals(target.replaceAll("_", ""))) {
+            return false;
+        }
+        for (int i = 0, j = 0, n = start.length(); i < n && j < n; ++i, ++j) {
+            while (i < n && start.charAt(i) == '_') {
+                ++i;
+            }
+            while (j < n && target.charAt(j) == '_') {
+                ++j;
+            }
+            if ((i < n) != (j < n)) {
+                return false;
+            }else if (i < n && j < n) {
+                char c = start.charAt(i), d = target.charAt(j); 
+                if (c != d || c == 'L' && i < j || c == 'R' && i > j) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
