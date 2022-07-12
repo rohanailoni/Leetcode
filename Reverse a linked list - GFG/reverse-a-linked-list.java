397bf38fea07b02a22f1047046feae70f5672bc4
rohanailoni <57516241+rohanailoni@users.noreply.github.com>
2022-07-12 07:21:13 +0530

    Added solution - LeetHub

---
+++ Reverse a linked list - GFG/reverse-a-linked-list.java
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+        
+        	int n = sc.nextInt();
+        
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+        
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+        
+            Solution ob = new Solution();
+            head = ob.reverseList(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   // } Driver Code Ends
+
+
+//function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int value;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution
+{
+    Node rever(Node head){
+        Node pre=null;
+        Node temp=null;
+        while(head!=null){
+            temp=head;
+            head=head.next;
+            temp.next=pre;
+            pre=temp;
+        }
+        return pre;
+    }
+    
+    Node reverseList(Node head)
+    {
+        return rever(head);
+    }
+}
