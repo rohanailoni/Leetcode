40218551ea7ea894ba41b7c59f3b19bf1dc2b107
rohanailoni <57516241+rohanailoni@users.noreply.github.com>
2022-07-18 16:34:05 +0530

    Time: 196 ms (72.46%), Space: 46 MB (72.70%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,7 +1,52 @@
 class Solution {
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
         int ans = 0;
-
+//         int n=matrix.length;
+//         int m=matrix[0].length;
+        
+//         int[][] dp=new int[n][m];//building the dp;
+//         dp[0][0]=matrix[0][0];//making the first value as default
+//         for(int i=1;i<m;i++){
+//             dp[0][i]=dp[0][i-1]+matrix[0][i];
+            
+//         }
+//         for(int i=1;i<n;i++){
+//             dp[i][0]=dp[i-1][0]+matrix[i][0];
+//         }
+//         for(int i=1;i<n;i++){
+//             for(int j=1;j<m;j++){
+//                 //System.out.println(dp[i][j-1]+" "+dp[i-1][j]+" "+dp[i-1][j-1]);
+//                 dp[i][j]=dp[i][j-1]+dp[i-1][j]+matrix[i][j]-dp[i-1][j-1];
+//             }
+//         }
+        // int max=Integer.MIN_VALUE;
+        // HashMap<Integer,Integer>map=new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         //map.put(dp[i][j],map.getOrDefault(dp[i][j],0)+1);
+        //         max=Math.max(dp[i][j],max);
+        //     }
+        // }
+        // for(int i=0;i<max;i++){
+        //     for(int j=0;j<max;j++){
+        //         for(int k=0;k<max;k++){
+        //             if(map.containsKey(i) && map.containsKey(j) && map.containsKey(k) && target==i+j-k){
+        //                 if(map.get(i)>0 && map.get(j)>0 && map.get(k)>0){
+        //                     int c=map.get(i);
+        //                     map.put(i,c--);
+        //                     c=map.get(j);
+        //                      map.put(j,c--);
+        //                     c=map.get(k);
+        //                      map.put(k,c--);
+        //                     if(i+j-k==target){
+        //                         ans++;
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        //System.out.println(Arrays.deepToString(dp));
         for(int i = 0; i<matrix[0].length;i++){//left col
             int[] sum = new int[matrix.length];
             for(int m = i; m<matrix[0].length;m++){//right col
@@ -18,7 +63,22 @@ class Solution {
                 }
             }
         }
-        
+        //System.out.println(map);
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 int basex=i;
+//                 int basey=j;
+//                 for(int al=0;al<n;al++){
+//                     for(int be=0;be<m;be++){
+//                         if(al==0){
+//                             if(basex==0 && basey==0){
+                                
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
         return ans;
 
     }
