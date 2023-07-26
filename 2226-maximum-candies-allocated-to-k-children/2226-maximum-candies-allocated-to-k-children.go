f33c81116f1043fbf8d0610cb157f99654056bbf
Rohan <57516241+rohanailoni@users.noreply.github.com>
2023-07-26 20:25:02 +0530

    Time: 142 ms (100.00%), Space: 9.3 MB (50.00%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.go
@@ -0,0 +1,39 @@
+func maximumCandies(candies []int, k int64) int {
+    n:= len(candies);
+    s:=sum(candies);
+    req:=int(math.Ceil(float64(s)/float64(k)));
+    low:=1;
+    high:=req;
+    ans:=int64(0)
+    for low<=high{
+        mid:= (high+low)/2;
+        got:=calc(mid,candies,n);
+        //fmt.Println(low,high,mid,ans)
+        if got>=k{
+            ans=int64(mid);
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    return int(ans);   
+}
+
+
+func sum(can []int)(int){
+    n:=len(can);
+    ans:=0;
+    for i:=0;i<n;i++{
+        ans+=can[i];
+    }
+    return ans;
+}
+func calc(k int,can []int,len int)int64{
+    total:=0;
+    for i:=0;i<len;i++{
+        rem:=can[i]/k;
+        //fmt.Println("for get",k,rem)
+        total+=rem;
+    }
+    return int64(total);
+}
