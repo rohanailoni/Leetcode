677d41669900f43f2ff431d45fe433ae86055fc4
Rohan <57516241+rohanailoni@users.noreply.github.com>
2023-08-29 10:15:34 +0530

    Time: 9 ms (66.67%), Space: 7.7 MB (22.22%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.go
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.go
@@ -1,23 +1,23 @@
 func bestClosingTime(customers string) int {
     n := len(customers)
-    prefixY := make([]int, n)
+    //prefixY := make([]int, n)
     prefixN := make([]int, n)
     
     if customers[0] == 'Y' {
-        prefixY[0] = 1
+        //prefixY[0] = 1
         prefixN[0] = 0
     } else {
-        prefixY[0] = 0
+        //prefixY[0] = 0
         prefixN[0] = 1
     }
     
     for i := 1; i < n; i++ {
         if customers[i] == 'Y' {
-            prefixY[i] = prefixY[i-1] + 1
+            //prefixY[i] = prefixY[i-1] + 1
             prefixN[i] = prefixN[i-1]
         } else {
             prefixN[i] = prefixN[i-1] + 1
-            prefixY[i] = prefixY[i-1]
+            //prefixY[i] = prefixY[i-1]
         }
     }
     
@@ -27,12 +27,15 @@ func bestClosingTime(customers string) int {
     for i := 0; i <= n; i++ {
         var temp int;
         if i>0 && i!=n{
-            temp = prefixN[i-1] + (prefixY[n-1] - prefixY[i-1]);
+            //temp = prefixN[i-1] + (prefixY[n-1] - prefixY[i-1]);
+            temp=prefixN[i-1]+(n-i-(prefixN[n-1]-prefixN[i-1]));
         }else{
-            temp=prefixY[n-1]
+            //temp=prefixY[n-1]
+            temp=n-prefixN[n-1];
         }
         if i==n{
             temp=prefixN[n-1];
+            //temp=prefixY[]
         }
         //fmt.Println(temp)
         if ans > float64(temp) {
